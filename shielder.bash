ef49a6f21b49fa0df4d92e8b2119aedd54a8a165
Giorgos Atmatsidis <georgatma.gr@gmail.com>
2021-08-22 10:14:20 +0300

    Update shielder.bash

--- shielder.bash
+++ shielder.bash
@@ -7,7 +7,6 @@ So anyone with a bruteforce method can try a combination of passwords to gain ac
 One action we can take is to lock ssh root and create a new system user with sudo privileges.
 Give new username in the empty space below
 [IMPORTANT]: Use strong username/passcode for SSH
-[*] Backup of sshd_config automatically created"
 
 read newuser
 sudo adduser $newuser
